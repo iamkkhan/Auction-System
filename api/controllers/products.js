9b49bff7cbc53f65d369680ff2a0492aecf00026
SaifRehman99 <saifr7493@gmail.com>
2019-09-19 14:23:05 +0500

    Saif changes

--- api/controllers/products.js
+++ api/controllers/products.js
@@ -1,183 +1,178 @@
 // importing the product model here
 const Product = require('../models/products');
+const mongoose = require('mongoose');
+const path = require('path');
+
 
 exports.getAllProducts = (req, res, next) => {
-  // finding all the documents here
-  Product.find()
-    .select('_id name price productImage')
-    .exec()
-    .then(doc => {
-      if (doc.length > 0) {
-        const response = {
-          count: doc.length,
-          // to modify the items we get from the DB, pass into a new array and add the new things accordingly
-          products: doc.map(item => {
-            return {
-              name: item.name,
-              price: item.price,
-              _id: item._id,
-              productImage: item.productImage,
-              request: {
-                type: 'GET',
-                url: `http://localhost:9000/products/${item._id}`
-              }
-            };
-          })
-        };
+    // finding all the documents here
+    Product.find()
+        .select('_id name price productImage')
+        .exec()
+        .then(doc => {
+            if (doc.length > 0) {
+                const response = {
+                    count: doc.length,
+                    // to modify the items we get from the DB, pass into a new array and add the new things accordingly
+                    products: doc.map(item => {
+                        return {
+                            name: item.name,
+                            price: item.price,
+                            _id: item._id,
+                            productImage: item.productImage,
+                            request: {
+                                type: 'GET',
+                                url: `http://localhost:9000/products/${item._id}`
+                            }
+                        };
+                    })
+                };
 
-        res.render('pages/index', {
-          res: doc
-        });
-        // dek yahn index render ki tou /products p uthaya usne
-        // age me index kru tou nh utha rha ha
-        // ap dekh addproduct tou me dalrha hu button p usme bh yehi same error derha ha na mean ye k route ka bhand ha
+                res.render('pages/index', {
+                    res: doc
+                });
 
-        // res.status(200).json(response);
-      } else {
-        res.status(404).json({
-          message: 'No Data Found Sir!'
+                // res.status(200).json(response);
+            } else {
+                res.status(404).json({
+                    message: 'No Data Found Sir!'
+                });
+            }
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: err
+            });
         });
-      }
-    })
-    .catch(err => {
-      res.status(500).json({
-        message: err
-      });
-    });
 };
 
 exports.getAddProduct = (req, res, next) => {
-  res.render('pages/addProducts');
+    res.render('pages/addProducts');
 };
 
 exports.addProducts = (req, res, next) => {
-  console.log(req.file);
-  console.log(req.local);
+    console.log(req.file);
+    console.log(req.local);
 
-  // creating the instant here
-  const products = new Product({
-    _id: new mongoose.Types.ObjectId(),
-    name: req.body.name,
-    price: req.body.price,
-    description: req.body.description,
-    productImage: req.file.path
-  });
-
-  Product.create(products, (err, newProd) => {
-    if (err) {
-      console.log(err);
-    } else {
-      res.render('/');
-      console.log('Hello');
-    }
-  });
-
-  // saving into the DB here
-  products
-    .save()
-    .then(result => {
-      res.status(201).json({
-        message: 'Order Added!',
-        Product: {
-          name: result.name,
-          price: result.price,
-          _id: result._id,
-          productImage: result.productImage,
-          request: {
-            type: 'POST',
-            url: `http://localhost:9000/products/${result._id}`
-          }
-        }
-      });
-    })
-    .catch(err => {
-      res.status(500).json({
-        message: err
-      });
+    // creating the instant here
+    const products = new Product({
+        _id: new mongoose.Types.ObjectId(),
+        name: req.body.name,
+        price: req.body.price,
+        description: req.body.description,
+        productImage: req.file.path
     });
+
+    Product.create(products, (err, newProd) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render('/');
+        }
+    });
+
+    // saving into the DB here
+    products
+        .save()
+        .then(result => {
+            res.status(201).json({
+                message: 'Order Added!',
+                Product: {
+                    name: result.name,
+                    price: result.price,
+                    _id: result._id,
+                    request: {
+                        type: 'POST',
+                        url: `http://localhost:9000/products/${result._id}`
+                    }
+                }
+            });
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: err
+            });
+        });
 };
 
 exports.getSingleProducts = (req, res, next) => {
-  // getting the id here
-  const ID = req.params.productId;
+    // getting the id here
+    const ID = req.params.productId;
 
-  Product.findById(ID)
-    .select('_id name price productImage')
-    .exec()
-    .then(doc => {
-      res.render('pages/products', {
-        singleRes: doc
-      });
+    Product.findById(ID)
+        .select('_id name price productImage')
+        .exec()
+        .then(doc => {
+            res.render('pages/products', {
+                singleRes: doc
+            });
 
-      res.status(200).json({
-        product: doc,
-        request: {
-          type: 'GET',
-          url: `http://localhost:9000/products`
-        }
-      });
-    })
-    .catch(err => {
-      res.status(500).json({
-        message: err
-      });
-    });
+            res.status(200).json({
+                product: doc,
+                request: {
+                    type: 'GET',
+                    url: `http://localhost:9000/products`
+                }
+            });
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: err
+            });
+        });
 };
 
 exports.updateProducts = (req, res, next) => {
-  // getting the id here
-  const ID = req.params.productID;
+    // getting the id here
+    const ID = req.params.productID;
 
-  // making an array of the update value here
-  const updateOp = {};
-  for (const op of req.body) {
-    updateOp[op.propName] = op.value;
-  }
-
-  Product.update(
-    {
-      _id: ID
-    },
-    {
-      $set: updateOp
+    // making an array of the update value here
+    const updateOp = {};
+    for (const op of req.body) {
+        updateOp[op.propName] = op.value;
     }
-  )
-    .exec()
-    .then(results => {
-      res.status(200).json({
-        message: `Product Updated!`,
-        request: {
-          type: 'PATCH',
-          url: `http://localhost:9000/products/${ID}`
-        }
-      });
-    })
-    .catch(err => {
-      res.status(500).json({
-        message: err
-      });
-    });
+
+    Product.update({
+            _id: ID
+        }, {
+            $set: updateOp
+        })
+        .exec()
+        .then(results => {
+            res.status(200).json({
+                message: `Product Updated!`,
+                request: {
+                    type: 'PATCH',
+                    url: `http://localhost:9000/products/${ID}`
+                }
+            });
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: err
+            });
+        });
 };
 
 exports.deleteProducts = (req, res, next) => {
-  // getting the id here
-  const ID = req.params.productID;
-  Product.remove({
-    _id: ID
-  })
-    .exec()
-    .then(doc => {
-      res.status(200).json({
-        message: 'Item Deleted!'
-      });
-    })
-    .catch(err => {
-      res.status(500).json({
-        message: 'No data found!',
-        request: {
-          type: 'DELETE',
-          url: `http://localhost:9000/products`
-        }
-      });
-    });
+    // getting the id here
+    const ID = req.params.productID;
+    Product.remove({
+            _id: ID
+        })
+        .exec()
+        .then(doc => {
+            res.status(200).json({
+                message: 'Item Deleted!'
+            });
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: 'No data found!',
+                request: {
+                    type: 'DELETE',
+                    url: `http://localhost:9000/products`
+                }
+            });
+        });
 };
--- api/routes/products.js
+++ api/routes/products.js
@@ -6,26 +6,26 @@ const productController = require('../controllers/products');
 // get request, getting all the Products in the database here
 router.get('/', productController.getAllProducts);
 
-router.get('/products/add-product', checkAuth, productController.getAddProduct);
+router.get('/products/add-product', productController.getAddProduct);
 
 // post request, adding the data in the DB storage
-router.post('/products/add-product', checkAuth, productController.addProducts);
+router.post('/products/add-product', productController.addProducts);
 
 // get request for the single users only
 router.get('/products/:productId', productController.getSingleProducts);
 
 // update request for the single users
 router.patch(
-  '/products/edit-product/:productId',
-  checkAuth,
-  productController.updateProducts
+    '/products/edit-product/:productId',
+    checkAuth,
+    productController.updateProducts
 );
 
 // delete request for the single users
 router.delete(
-  '/products/delete-product/:productID',
-  checkAuth,
-  productController.deleteProducts
+    '/products/delete-product/:productID',
+    checkAuth,
+    productController.deleteProducts
 );
 
 module.exports = router;
