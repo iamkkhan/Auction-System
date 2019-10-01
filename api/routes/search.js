a94806eaa815d94651870808b3d41c6277b2c011
SaifRehman99 <saifr7493@gmail.com>
2019-10-01 19:47:34 +0500

    Pattern[REGX]

--- api/routes/search.js
+++ api/routes/search.js
@@ -6,7 +6,13 @@ const checkAuth = require("../middleware/check_auth");
 
 // searhcing the products here
 router.get("/search-product", (req, res, next) => {
-    Product.find({ Category: req.query.search })
+    // getting the querry here
+    let queryWord = req.query.search;
+
+    // changing into upper case
+    const upper = queryWord.replace(/^\w/, c => c.toUpperCase());
+
+    Product.find({ Category: upper })
         .then(docs => {
             if (docs.length < 1) {
                 res.render("pages/noProducts", {
@@ -15,6 +21,7 @@ router.get("/search-product", (req, res, next) => {
             } else {
                 var getDate = moment(docs.date).format("YYYY, MM, D,  h:mm:ss a");
                 var dateFromNow = moment(getDate).fromNow();
+
                 res.render("pages/searchProducts", {
                     search: req.query.search,
                     data: docs,
